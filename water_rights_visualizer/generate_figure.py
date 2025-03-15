Fixes csv overwrite bug, applies daylight calculations per day instead of as a monthly average, standardizes ET colorscale across years, re-calculates all PNGs at end

water_rights_visualizer/generate_figure.py
@@ -51,6 +51,7 @@ def generate_figure(
     text_panel: ScrolledText = None,
     image_panel: Text = None,
     status_filename: str = None,
+    requestor: dict[str, str] = None,
     metric_units: bool = True,
 ):
     """
@@ -90,23 +91,32 @@ def generate_figure(
     max_length_short = 15
     max_length_medium = 30
 
+    requestor_name = ""
+    if requestor:
+        requestor_name = requestor.get("name", "")
+        if not requestor_name:
+            requestor_name = requestor.get("email", "")
+        if not requestor_name:
+            requestor_name = requestor.get("sub", "")
+
+    if not requestor_name:
+        requestor_name = "Unknown"
+
     if len(ROI_name) <= max_length_short:
         title = f"Evapotranspiration For {ROI_name}"
-        subtitle = f"Year: {year}\nArea: {ROI_acres} acres\nCreated: {creation_date.date()}"
+        subtitle = f"Year: {year}\nArea: {ROI_acres} acres\nCreated: {creation_date.date()}\nRequested By: {requestor_name}"
     elif len(ROI_name) <= max_length_medium:
         title_fontsize = 12
         title = f"Evapotranspiration For {ROI_name}"
-        subtitle = f"Year: {year}\nArea: {ROI_acres} acres\nCreated: {creation_date.date()}"
+        subtitle = f"Year: {year}\nArea: {ROI_acres} acres\nCreated: {creation_date.date()}\nRequested By: {requestor_name}"
     else:
         title_fontsize = 12
         short_name = ROI_name[:max_length_medium] + "..."
         title = f"Evapotranspiration For {short_name}"
-        subtitle = f"Year: {year}\nArea: {ROI_acres} acres\nCreated: {creation_date.date()}"
+        subtitle = f"Year: {year}\nArea: {ROI_acres} acres\nCreated: {creation_date.date()}\nRequested By: {requestor_name}"
 
-    # Add the title
-    # align left
     fig.suptitle(title, fontsize=title_fontsize, ha="left", x=0.125, fontweight="bold")
-    fig.text(0.125, 0.91, subtitle, fontsize=axis_label_fontsize, ha="left", fontweight="normal")
+    fig.text(0.125, 0.895, subtitle, fontsize=axis_label_fontsize, ha="left", fontweight="normal")
 
     n_months = end_month - start_month + 1
     grid_cols = int(n_months / 3)
@@ -209,8 +219,12 @@ def generate_figure(
 
     df = main_df[main_df["Year"] == year]
     x = df["Month"]
-    y = df["PET"] if metric_units else mm_to_in(df["PET"])
-    y2 = df["ET"] if metric_units else mm_to_in(df["ET"])
+
+    df["ET"] = df["ET"] if metric_units else mm_to_in(df["ET"])
+    df["PET"] = df["PET"] if metric_units else mm_to_in(df["PET"])
+
+    y = df["PET"]
+    y2 = df["ET"]
 
     if "ppt_avg" in df.columns:
         df["ppt_avg"] = df["ppt_avg"] if metric_units else mm_to_in(df["ppt_avg"])
@@ -271,10 +285,9 @@ def generate_figure(
         # y = np.where(y < y2, df["et_ci_ymax"], y)
         y = np.maximum(y, df["et_ci_ymax"])
         y = np.maximum(y, y2)
-
-    # Go through et_ci and make sure ymin is less than y2 and ymax is greater than y2, if not, set et_ci to y2
-    df["et_ci_ymin"] = np.where(df["et_ci_ymin"] < y2, df["et_ci_ymin"], y2)
-    df["et_ci_ymax"] = np.where(df["et_ci_ymax"] > y2, df["et_ci_ymax"], y2)
+        # Go through et_ci and make sure ymin is less than y2 and ymax is greater than y2, if not, set et_ci to y2
+        df["et_ci_ymin"] = np.where(df["et_ci_ymin"] < y2, df["et_ci_ymin"], y2)
+        df["et_ci_ymax"] = np.where(df["et_ci_ymax"] > y2, df["et_ci_ymax"], y2)
 
     sns.lineplot(x=x, y=y, ax=ax, color=pet_color, label=pet_label, marker=marker, markersize=marker_size)
     sns.lineplot(x=x, y=y2, ax=ax, color=et_color, label="ET", marker=marker, markersize=marker_size)
@@ -298,10 +311,10 @@ def generate_figure(
         "Precipitation": {"color": ppt_color, "alpha": 0.8, "lw": 2},
     }
 
-    if int(year) < OPENET_TRANSITION_DATE:
+    if int(year) < OPENET_TRANSITION_DATE or is_ensemble_range_data_null:
         del legend_items["Ensemble Min/Max"]
+
     if is_ensemble_range_data_null:
-        del legend_items["Ensemble Min/Max"]
         legend_items["Ensemble Min/Max (Unavailable)"] = {"color": et_color, "alpha": 0.1, "lw": 4}
 
     legend_labels = legend_items.keys()

water_rights_visualizer/process_monthly.py
@@ -74,18 +74,17 @@ def process_monthly(
             ET_month_stack = ET_stack[start_index:end_index, :, :]
             ET_monthly = np.nansum(ET_month_stack, axis=0)
 
-            logger.info(f"writing monthly ET: {ET_monthly_filename}")
+            logger.info(f"writing monthly ET: {ET_monthly_filename}, avg: {ET_monthly.mean()}")
             subset_geometry = rt.RasterGrid.from_affine(subset_affine, rows, cols, CRS)
             ET_monthly_raster = rt.Raster(array=ET_monthly, geometry=subset_geometry)
             ET_monthly_raster.to_geotiff(ET_monthly_filename)
 
             PET_monthly_filename = join(monthly_sums_directory, f"{year:04d}_{month:02d}_{ROI_name}_PET_monthly_sum.tif")
 
-            logger.info(f"end index: {end_index}")
             PET_month_stack = PET_stack[start_index:end_index, :, :]
             PET_monthly = np.nansum(PET_month_stack, axis=0)
 
-            logger.info(f"writing monthly PET: {PET_monthly_filename}")
+            logger.info(f"writing monthly PET: {PET_monthly_filename}, avg: {PET_monthly.mean()}")
             subset_geometry = rt.RasterGrid.from_affine(subset_affine, rows, cols, CRS)
             PET_monthly_raster = rt.Raster(array=PET_monthly, geometry=subset_geometry)
             PET_monthly_raster.to_geotiff(PET_monthly_filename)
@@ -95,6 +94,15 @@ def process_monthly(
 
             ET_monthly_mean = np.nanmean(ET_values) if len(ET_values) > 0 else 0
             PET_monthly_mean = np.nanmean(PET_values) if len(PET_values) > 0 else 0
+
+            # Remove mask from the mean calculation if we don't have any (small ROI). This is ONLY done for PET as this is an upper bound
+            # and PET is lower resolution for later years
+            if PET_monthly_mean == 0:
+                logger.warning(
+                    f"Monthly PET mean is zero for year {year}, month {month}. Expanding the mask to include the full subset."
+                )
+                PET_monthly_mean = np.nanmean(np.array(PET_monthly).flatten()) or 0
+                logger.info(f"New PET monthly mean: {PET_monthly_mean}, PET value count: {len(PET_values)}")
 
             monthly_means.append([year, month, ET_monthly_mean, PET_monthly_mean])
 