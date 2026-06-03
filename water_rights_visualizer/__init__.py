8282a2a72f8a385efc246ecedf475d3445d5d2d7
Ryan Stonebraker <ryanastonebraker@gmail.com>
2026-06-03 12:50:08 -0700

    Lazy loads pystac so we dont have to load for unit tests

--- water_rights_visualizer/__init__.py
+++ water_rights_visualizer/__init__.py
@@ -1,10 +1,15 @@
-from .landsat_pass_count import count_landsat_passes_for_month
+__all__ = ["water_rights_visualizer", "count_landsat_passes_for_month"]
+
+
+def __getattr__(name: str):
+    if name == "count_landsat_passes_for_month":
+        from .landsat_pass_count import count_landsat_passes_for_month
+
+        return count_landsat_passes_for_month
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
 
 
 def water_rights_visualizer(*args, **kwargs):
     from .water_rights_visualizer import water_rights_visualizer as run_water_rights_visualizer
 
     return run_water_rights_visualizer(*args, **kwargs)
-
-
-__all__ = ["water_rights_visualizer", "count_landsat_passes_for_month"]
